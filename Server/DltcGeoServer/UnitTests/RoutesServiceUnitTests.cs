d0170a6f0888222ff9af9f43743d9719132d5842
Anna Shaleva <shaleva.ann@gmail.com>
2019-11-21 12:32:25 +0300

    Added initial UnitTests project

---
+++ Server/DltcGeoServer/UnitTests/RoutesServiceUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using DltcGeoServer.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class RoutesServiceUnitTests
+    {
+        private RoutesService _routesService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _routesService = new RoutesService();
+        }
+    }
+}
