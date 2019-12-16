8357a0370ac4bfb1344126bdeea3707dac3910bc
Anna Shaleva <shaleva.ann@gmail.com>
2019-12-16 15:06:44 +0300

    GeoJson helpers

---
+++ Server/DltcGeoServer/DltcGeoServer/Models/CustomFeature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DltcGeoServer.Models
+{
+    public class CustomFeature
+    {
+        public string type => "Feature";
+        public Geometry geometry;
+        public List<Property> properties => new List<Property>();
+    }
+}
---
+++ Server/DltcGeoServer/DltcGeoServer/Models/Property.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DltcGeoServer.Models
+{
+    public class Property
+    {
+        public string Prop0 => "value0";
+    }
+}
